c013edeff62bbac3171dc7c5ea88f1706edd8eab
Janek Nouvertné <provinzkraut@posteo.de>
2026-06-14 16:43:11 +0200

    first attempt

---
+++ tests/prof/perf/test_encode_decode.py
@@ -0,0 +1,64 @@
+import pytest
+
+from benchmarks.bench_encodings import Directory
+from benchmarks.bench_validation import bench_msgspec
+from benchmarks.generate_data import make_filesystem_data
+
+import msgspec.json
+import msgspec.msgpack
+from pytest_codspeed import BenchmarkFixture
+
+
+@pytest.fixture(params=[1000, 10000])
+def file_system_data(request):
+    return make_filesystem_data(request.param)
+
+
+json_decoder = msgspec.json.Decoder(type=Directory)
+json_encoder = msgspec.json.Encoder()
+
+msgpack_decoder = msgspec.json.Decoder(type=Directory)
+msgspec_encoder = msgspec.json.Encoder()
+
+
+@pytest.mark.parametrize(
+    "encoder",
+    [
+        pytest.param(json_encoder.encode, id="json"),
+        pytest.param(msgspec_encoder.encode, id="msgpack"),
+    ],
+)
+def test_encode(benchmark: BenchmarkFixture, encoder, file_system_data):
+    res = benchmark(encoder, file_system_data)
+
+    assert isinstance(res, bytes)
+
+
+@pytest.mark.parametrize(
+    "encoder,decoder",
+    [
+        pytest.param(json_encoder.encode, json_decoder.decode, id="json"),
+        pytest.param(msgspec_encoder.encode, msgpack_decoder.decode, id="msgpack"),
+    ],
+)
+def test_decode(benchmark: BenchmarkFixture, encoder, decoder, file_system_data):
+    data = encoder(file_system_data)
+    res = benchmark(decoder, data)
+
+    assert isinstance(res, Directory)
+
+
+@pytest.mark.parametrize(
+    "encoder,decoder",
+    [
+        pytest.param(json_encoder.encode, json_decoder.decode, id="json"),
+        pytest.param(msgspec_encoder.encode, msgpack_decoder.decode, id="msgpack"),
+    ],
+)
+def test_roundtrip(benchmark: BenchmarkFixture, encoder, decoder, file_system_data):
+    def func(data):
+        return decoder(encoder(data))
+
+    res = benchmark(func, file_system_data)
+
+    assert isinstance(res, Directory)
